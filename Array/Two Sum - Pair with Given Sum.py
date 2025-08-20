3ece569f1e1738fe5e4d199b638972a1574624f0
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2025-08-21 00:16:11 +0530

    Create Two Sum - Pair with Given Sum.py

---
+++ Array/Two Sum - Pair with Given Sum.py
@@ -0,0 +1,25 @@
+class Solution:
+	def twoSum(self, arr, target):
+		# code here
+		
+		#Brute force approach
+# 		for i in range(0,len(arr)-1):
+# 		    for j in range(i+1,len(arr)):
+# 		        if(arr[i]+arr[j]==target):
+# 		            return True
+# 		return False
+        
+        #it will track value of the array with its index
+        hash_map = {}
+        
+        #no pair is there
+        if(len(arr)==1):
+            return False
+        
+        for i in range(0,len(arr)):
+            second=target-arr[i]
+            if(second in hash_map):
+                return True
+            else:
+                hash_map[arr[i]]=i
+        return False
